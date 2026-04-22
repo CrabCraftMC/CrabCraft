7f8bc08612eb96121d5d77a252f6912bcb9e3a0b
Max Moon <max@maxmoon.sh>
2026-04-22 22:11:45 +0100

    Add alt account CRUD queries and bot re-exports

---
+++ apps/bot/src/utils/altDb.ts
@@ -0,0 +1,10 @@
+export {
+  addPlayerAlt,
+  removePlayerAlt,
+  getPlayerAlts,
+  getAltCountForUser,
+  deleteAllAltsForUser,
+  isAltUuidTaken,
+  MAX_ALTS,
+  type PlayerAlt,
+} from "@crabcraft/db/queries/bot";
